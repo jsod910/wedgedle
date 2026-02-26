3a29e439364af73c0219b6fed9fa045f69a370fb
Jacob <jsodini3@illinois.edu>
2026-02-26 01:21:33 -0600

    debug statements inside app.py

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
+print("STEP 1: Starting app import", flush=True)
+
 from flask import Flask, render_template
+print("STEP 1: Starting app import", flush=True)
+
 from flask_cors import CORS
+print("STEP 2: Flask imported", flush=True)
+
 # import backend functions
 from games.wedgedle.routes import wedgedle_bp
+print("STEP 3: CORS imported", flush=True)
 from games.shipdle.routes import shipdle_bp
+print("STEP 4: Wedgedle blueprint imported", flush=True)
 
 app = Flask(__name__)
+print("STEP 5: Shipdle blueprint imported", flush=True)
 CORS(app)
+print("STEP 7: CORS applied", flush=True)
 
 app.register_blueprint(wedgedle_bp, url_prefix="/wedgedle")
+print("STEP 7: CORS applied", flush=True)
 app.register_blueprint(shipdle_bp, url_prefix="/shipdle")
+print("STEP 8: Wedgedle blueprint registered", flush=True)
 
 @app.route("/")
 def landing():
